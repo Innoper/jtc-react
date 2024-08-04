1f113591873430bc72f09b8faa7c29b4670ed893
Innoper <teaminnoper@gmail.com>
2024-08-04 15:10:47 +0900

    Feat: TextProps inteface 생성

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,6 +2,7 @@ import React from "react";
 
 export interface TextProps {
   children: string;
+  style?: React.CSSProperties;
   fontSize?: React.CSSProperties["fontSize"];
   color?: React.CSSProperties["color"];
   fontWeight?: React.CSSProperties["fontWeight"];
