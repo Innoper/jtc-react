68b7e867ebb199365f4de662dc606bb9e72056b1
Innoper <teaminnoper@gmail.com>
2024-08-08 03:10:59 +0900

    Feat: Wave 타입 JTC Checkbox 컴포넌트 생성,적용

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -1,11 +1,13 @@
 import { CheckboxProps } from "../types";
 import { DefaultCheckboxDiv } from "./CheckboxType/DefaultCheckbox";
+import { WaveCheckboxDiv } from "./CheckboxType/WaveCheckbox";
 
 /**
  * JTC Checkbox Component
  */
 export default function Checkbox({
   id,
+  type,
   icon,
   width = "15px",
   height = "15px",
@@ -14,9 +16,39 @@ export default function Checkbox({
   disabled = false,
   checked = false,
 }: CheckboxProps) {
-  const inputClassName = "jtc-checkbox-input";
-  const labelClassName = "jtc-checkbox-label";
-  const spanClassName = "jtc-checkbox-span";
+  const inputClassName = type
+    ? `jtc-checkbox-input-${type}`
+    : "jtc-checkbox-input";
+  const labelClassName = type
+    ? `jtc-checkbox-label-${type}`
+    : "jtc-checkbox-label";
+  const spanClassName = type
+    ? `jtc-checkbox-span-${type}`
+    : "jtc-checkbox-span";
+
+  if (type === "wave") {
+    return (
+      <WaveCheckboxDiv width={width} height={height} color={color}>
+        <input
+          className={inputClassName}
+          id={`jtc-checkbox-${id}`}
+          type="checkbox"
+          onChange={onChange}
+          disabled={disabled}
+          checked={checked}
+        />
+        <label className={labelClassName} htmlFor={`jtc-checkbox-${id}`}>
+          <span>
+            {icon ? (
+              <img className="jtc-checkbox-img" src={icon} />
+            ) : (
+              <img className="jtc-checkbox-img" src="/public/path.svg" />
+            )}
+          </span>
+        </label>
+      </WaveCheckboxDiv>
+    );
+  }
 
   return (
     <DefaultCheckboxDiv width={width} height={height} color={color}>
