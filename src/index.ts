554d81100b56b3769f7fba5a2bb56997abb7d1d8
Innoper <teaminnoper@gmail.com>
2024-08-08 03:22:47 +0900

    Feat: export JTC Checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import CardLayout from "./Card/CardLayout";
+import Checkbox from "./Checkbox/Checkbox";
 import Item from "./Item/Item";
 import Text from "./Text/Text";
-import { TextProps, CardLayoutProps, ItemProps } from "./types";
+import { TextProps, CardLayoutProps, ItemProps, CheckboxProps } from "./types";
 
 const JTC = {
   Text,
   CardLayout,
   Item,
+  Checkbox,
 };
 
 export default JTC;
 export type { TextProps };
 export type { CardLayoutProps };
 export type { ItemProps };
+export type { CheckboxProps };
